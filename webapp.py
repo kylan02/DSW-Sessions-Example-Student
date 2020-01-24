084f5731389da442becb38727fd977f098de05ef
kylan02 <31520026+kylan02@users.noreply.github.com>
2020-01-24 09:56:21 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -39,6 +39,7 @@ def renderPage2():
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
     #TODO: save the favorite color in the session
+    session["fav_color"] = request.form["favoriteColor"]
     
     return render_template('page3.html')
     
